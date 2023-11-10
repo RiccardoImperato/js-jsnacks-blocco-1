b49c9b5c1ae5a8a06fc902b06a00e443673379ac
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-10 14:45:32 +0100

    fix: correzione snack2 e snack3

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -3,17 +3,22 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-const numeroUno = Number(prompt('Inserisci un numero'));
-const numeroDue = Number(prompt('Inserisci un numero'));
-const numeroTre = Number(prompt('Inserisci un numero'));
-const numeroQuattro = Number(prompt('Inserisci un numero'));
-const numeroCinque = Number(prompt('Inserisci un numero'));
-const numeroSei = Number(prompt('Inserisci un numero'));
-const numeroSette = Number(prompt('Inserisci un numero'));
-const numeroOtto = Number(prompt('Inserisci un numero'));
-const numeroNove = Number(prompt('Inserisci un numero'));
-const numeroDieci = Number(prompt('Inserisci un numero'));
+let somma = 0;
+let count = 10;
 
-const somma = numeroUno + numeroDue + numeroTre + numeroQuattro + numeroCinque + numeroSei + numeroSette + numeroOtto + numeroNove + numeroDieci
+for (let i = 1; i <= count; i++) {
+    const numeroUtente = Number(prompt(`${i} 'Inserici un numero'`));
+
+    if (!isNaN(numeroUtente)) {
+        console.log(`${i} - Hai inserito il numero: ${numeroUtente}`);
+        somma += numeroUtente;
+        console.log(`${i} La somma a questo punto è: ${somma} `)
+    }
+    else {
+        console.log(`${i} Hai inserito un valore non valido`)
+    }
+
+}
+
 
 console.log(`La somma dei numeri inseriti è: ${somma}`)
