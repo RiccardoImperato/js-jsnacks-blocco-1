3bfc1e964f1d0b47ad97058331f7501a0a72ad90
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-10 11:32:28 +0100

    feat: condizione parole stessa lunghezza

--- snack2/main.js
+++ snack2/main.js
@@ -8,9 +8,12 @@ const parolaUno = (prompt('Scrivi la prima parola'));
 const parolaDue = (prompt('Scrivi la seconda parola'));
 
 // Confronto
-if (parolaUno < parolaDue) {
+if (parolaUno.length < parolaDue.length) {
     console.log(parolaUno);
     console.log(parolaDue);
+}
+else if (parolaUno.length === parolaDue.length) {
+    console.log('Le due parole hanno la stessa lunghezza');
 }
 else {
     console.log(parolaDue);
