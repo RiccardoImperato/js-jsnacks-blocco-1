a72d17429cc56cd4dff28da7ebe4af3f73454266
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-10 12:15:24 +0100

    feat: snack3

---
+++ snack3/js/main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+const numeroUno = Number(prompt('Inserisci un numero'));
+const numeroDue = Number(prompt('Inserisci un numero'));
+const numeroTre = Number(prompt('Inserisci un numero'));
+const numeroQuattro = Number(prompt('Inserisci un numero'));
+const numeroCinque = Number(prompt('Inserisci un numero'));
+const numeroSei = Number(prompt('Inserisci un numero'));
+const numeroSette = Number(prompt('Inserisci un numero'));
+const numeroOtto = Number(prompt('Inserisci un numero'));
+const numeroNove = Number(prompt('Inserisci un numero'));
+const numeroDieci = Number(prompt('Inserisci un numero'));
+
+const somma = numeroUno + numeroDue + numeroTre + numeroQuattro + numeroCinque + numeroSei + numeroSette + numeroOtto + numeroNove + numeroDieci
+
+console.log(`La somma dei numeri inseriti è: ${somma}`)
