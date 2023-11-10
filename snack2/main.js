89f19f09688b5a58a7754cb043031e4b1ee6337a
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-10 11:26:19 +0100

    feat: snack2

---
+++ snack2/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// Variabili parole
+const parolaUno = (prompt('Scrivi la prima parola'));
+const parolaDue = (prompt('Scrivi la seconda parola'));
+
+// Confronto
+if (parolaUno < parolaDue) {
+    console.log(parolaUno);
+    console.log(parolaDue);
+}
+else {
+    console.log(parolaDue);
+    console.log(parolaUno);
+}
